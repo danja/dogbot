e78519b534c264e331e422e5b6ceaf27eaab1ca4
Danny Ayers <danny.ayers@gmail.com>
2023-10-31 20:25:31 +0100

    tweak

--- src/lib/openai-connect.js
+++ src/lib/openai-connect.js
@@ -2,24 +2,6 @@ const OpenAI = require("openai");
 const openai = new OpenAI();
 
 module.exports.promptAI = async function (message) {
-  console.log('AI prompted with : ' + message)
-
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
-  })
-
-  const JSON = require('JSON')
-
-  aiResponse = completion.choices[0]
-  aiText = aiResponse['message']['content']
-  console.log('AI responds : ' + aiText)
-  return aiText
-}
-
-/*
-
-module.exports.promptAI = async function (message) {
   await promptAI(message);
 }
 
@@ -38,6 +20,10 @@ var promptAI = async function (message) {
   console.log('AI responds : ' + aiText)
   return aiText
 }
+
+/*
+
+
 */
 
 // AI responds : {"index":0,"message":{"role":"assistant","content":"How can I assist you today?"},"finish_reason":"stop"}
